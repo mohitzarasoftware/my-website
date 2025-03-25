726ba4a183b2f7cba04f4585a2aba3122d1a15b9
mohitzarasoftware <163724657+mohitzarasoftware@users.noreply.github.com>
2025-03-26 01:25:38 +0530

    sds

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,19 @@ window.addEventListener('scroll', function() {
     }
 });
 
+document.addEventListener("keydown", function (event) {
+  // Prevent F12 (Dev Tools)
+  if (event.key === "F12") {
+      event.preventDefault();
+  }
+  // Prevent Ctrl+Shift+I (Dev Tools), Ctrl+Shift+J (Console), Ctrl+Shift+C (Element Inspector)
+  if ((event.ctrlKey && event.shiftKey && event.key === "I") ||
+      (event.ctrlKey && event.shiftKey && event.key === "J") ||
+      (event.ctrlKey && event.shiftKey && event.key === "C")) {
+      event.preventDefault();
+  }
+  // Prevent Ctrl+U (View Source)
+  if (event.ctrlKey && event.key === "u") {
+      event.preventDefault();
+  }
+});
